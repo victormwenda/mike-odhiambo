d7fbf3fcc0a2de79765b1095c8ca153296aa99a6
Victor Mwenda <vmwenda.vm@gmail.com>
2017-03-16 11:30:39 +0300

    Basic ui, code cleanup etc

---
+++ assignment-3/views/site/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Assignment 3';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Greetings!</h1>
+
+        <p class="lead">Thank you for visiting my web page</p>
+
+        <p><a class="btn btn-lg btn-success" href="mailto:michailmik88@gmail.com">Leave me an email</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+
+        </div>
+    </div>
+</div>
